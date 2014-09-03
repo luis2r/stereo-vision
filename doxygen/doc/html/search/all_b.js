d30f756cbc1bd25b14eab6548c60d6f24397e47a
Ugo Pattacini <ugo.pattacini@iit.it>
2014-09-03 17:52:56 +0200

    provided doxygen documentation

---
+++ doxygen/doc/html/search/all_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['rfmodule_20to_20convert_20from_20rgb_20_2b_20depth_20map_20into_20pointcloud_2e',['RFModule to convert from RGB + Depth map into pointcloud.',['../md_C_1_dev_icub-contrib-iit_stereo-vision_modules_pointCloudConversion_Readme.html',1,'']]],
+  ['rectifyimages',['rectifyImages',['../classStereoCamera.html#ae5ac866c6d4b6c4819b01a918e7b61e0',1,'StereoCamera']]]
+];
---
+++ doxygen/doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rfmodule_20to_20convert_20from_20rgb_20_2b_20depth_20map_20into_20pointcloud_2e',['RFModule to convert from RGB + Depth map into pointcloud.',['../md_C_1_dev_icub-contrib-iit_stereo-vision_modules_pointCloudConversion_Readme.html',1,'']]]
+];
